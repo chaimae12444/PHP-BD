be2e52d5839437346290a13740750518859828cc
chaimae12444 <sedraoui.chaimae.solicode@gmail.com>
2026-03-31 12:11:03 +0100

    Add files via upload

---
+++ chapitre-4/connexion.php
@@ -0,0 +1,21 @@
+<?php
+$host = 'localhost';
+$dbname = 'blogdb';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base $dbname";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+try {
+    $pdo->query("SELECT * FROM table_inexistante");
+} catch (PDOException $e) {
+    echo "Erreur SQL : " . $e->getMessage();
+}catch (PDOException $e) {
+    file_put_contents('erreurs.log', $e->getMessage(), FILE_APPEND);
+    echo "Une erreur est survenue. Contactez l'administrateur.";
+}
